6383d06e74d259db9cb35d8d0fcff1e7fb15ca54
SamaraFlor <65172085+SamaraFlor@users.noreply.github.com>
2022-06-09 21:34:07 -0300

    Add files via upload

---
+++ funcoes.mjs
@@ -0,0 +1,16 @@
+function mostraIdade(nome, idade){
+    return 'Á idade de'      + nome +     'é' +   idade;
+}
+
+function mostraCidade(nome, cidade){
+    return 'a cidade de' +    nome +    'é'       +  cidade;
+}
+
+
+function mostraHobbie(nome, hobbie){
+    return 'o hobbie de ', +   nome   +    'é'   +    hobbie;
+}
+
+export { mostraIdade,
+         mostraCidade,
+         mostraHobbie};
---
+++ mains.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade,mostraCidade,mostraHobbie } from "./funcoes.mjs";
+
+console.log(mostraIdade("Samara",32))
+console.log(mostraCidade("Samara","São Paulo"));
+console.log(mostraHobbie("Samara", "ler livros"));
